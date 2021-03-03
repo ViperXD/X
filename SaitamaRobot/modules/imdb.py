95218684943135969a928f74c290022656d4005a
ALEN TL <67356843+ALENTL@users.noreply.github.com>
2021-03-03 11:51:57 +0530

    Update imdb.py

--- SaitamaRobot/modules/imdb.py
+++ SaitamaRobot/modules/imdb.py
@@ -6,8 +6,8 @@ import re
 import subprocess
 import time
 from datetime import datetime
-from SaitamaRobot.events import register
-from SaitamaRobot import LOGGER, client
+from SaitamaRobot.event import register
+from SaitamaRobot import LOGGER, telethn
 from telethon import types
 from telethon.tl import functions
 
@@ -17,13 +17,13 @@ async def is_register_admin(chat, user):
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
 
         return isinstance(
-            (await client(functions.channels.GetParticipantRequest(chat, user))).participant,
+            (await telethn(functions.channels.GetParticipantRequest(chat, user))).participant,
             (types.ChannelParticipantAdmin, types.ChannelParticipantCreator)
         )
     elif isinstance(chat, types.InputPeerChat):
 
-        ui = await client.get_peer_id(user)
-        ps = (await client(functions.messages.GetFullChatRequest(chat.chat_id))) \
+        ui = await telethn.get_peer_id(user)
+        ps = (await telethn(functions.messages.GetFullChatRequest(chat.chat_id))) \
             .full_chat.participants.participants
         return isinstance(
             next((p for p in ps if p.user_id == ui), None),
