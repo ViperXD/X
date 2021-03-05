783d44d751dd9760a3bb3652a95a8341752d3570
ALEN TL <67356843+ALENTL@users.noreply.github.com>
2021-03-05 08:34:13 +0530

    Added android module to work

--- SaitamaRobot/modules/android.py
+++ SaitamaRobot/modules/android.py
@@ -7,9 +7,9 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 from telegram.error import BadRequest
 from telegram.ext import run_async
 
-from Elizabeth import dispatcher
-from Elizabeth.modules.disable import DisableAbleCommandHandler
-from Elizabeth.modules.helper_funcs.alternate import typing_action
+from SaitamaRobot import dispatcher
+from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from SaitamaRobot.modules.helper_funcs.alternate import typing_action
 
 GITHUB = "https://github.com"
 DEVICES_DATA = "https://raw.githubusercontent.com/androidtrackers/certified-android-devices/master/by_device.json"
